9ee82857998053358b785be09d21f894f3373d3e
Ignacio A <105741412+ignacio-ave@users.noreply.github.com>
2023-03-30 22:14:51 -0300

    cambio de comentarios

--- arraylist.c
+++ arraylist.c
@@ -3,163 +3,112 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*En este laboratorio implementaremos una lista. Para ello usaremos la siguiente
- * estructura (arreglo no circular):*/
 typedef struct ArrayList {
-  void **data;  // un arreglo dinámico para almacenar punteros a los datos
-  int capacity; // el tamaño actual del arreglo
-  int size;     // la cantidad de datos válidos almacenados en la lista
-} ArrayList; // Se recomienda que el tamaño del arreglo (capacity) sea mayor a
-             // la cantidad de datos (size) de la lista. De esta manera, no es
-             // necesario expandir el arreglo cada vez que se insertan nuevos
-             // datos.
-
-/*
-Implemente la función ArrayList *createList(). Esta función crea un nuevo dato
-de tipo ArrayList inicializando sus variables. Considere que la capacidad incial
-es de 2 casillas para el arreglo.
-
-    Recuerde reservar memoria para el ArrayList y también para el arreglo
-dinámico data.
-*/
-ArrayList *createList(void) {
-  ArrayList *nuevo = (ArrayList *)malloc(sizeof(ArrayList));
-  nuevo->data = (void **)malloc(sizeof(void *) * 2);
+  void **data;  
+  int capacity; 
+  int size;    
+} ArrayList;
+             
+             
+ArrayList *createList(void) { // crear un arraylist vacio
+  ArrayList *nuevo = (ArrayList *)malloc(sizeof(ArrayList)); // reservar memoria para el arraylist
+  nuevo->data = (void **)malloc(sizeof(void *) * 2); // reservar memoria para el array de punteros
     
-  if (nuevo == NULL || nuevo->data == NULL)
+  if (nuevo == NULL || nuevo->data == NULL) // si no se pudo reservar memoria
     exit(1);
 
-  nuevo->capacity = 2;
-  nuevo->size = 0;
+  nuevo->capacity = 2; // capacidad inicial
+  nuevo->size = 0; // tamaño inicial
 
   
   return nuevo;
 }
-/*
-Implemente la función void append(ArrayList * l, void * data). Esta función
-agrega el dato al final de la lista (es decir, en la posición size).
 
-    Si el arreglo está lleno aumente la capacidad al doble usando la función
-realloc así: data = realloc(data, nueva_capacidad)
 
-    Recuerde aumentar el valor de la variable size.
-
-*/
-void doblarcapacity(ArrayList *l) {
-  l->data = realloc(l->data, sizeof(void *) * 2 * l->capacity);
-  if (l->data == NULL)
+void doblarcapacity(ArrayList *l) {  // doblar la capacidad del array
+  l->data = realloc(l->data, sizeof(void *) * 2 * l->capacity); // doblar la capacidad del array
+  if (l->data == NULL) // si no se pudo reservar memoria
     return;
-  l->capacity *= 2;
+  l->capacity *= 2; // doblar la capacidad
 }
 
-void append(ArrayList *l, void *data) {
-  if (l->size == l->capacity)
+void append(ArrayList *l, void *data) { // agregar un elemento al final del array
+  if (l->size == l->capacity) // si el array esta lleno
     doblarcapacity(l);
-  l->data[l->size] = data;
-  l->size += 1;
+  l->data[l->size] = data; // agregar el elemento al final del array
+  l->size += 1; // aumentar el tamaño del array
 }
 
 
-/*
-
-Implemente la función void push(ArrayList * l, void * data, int i). Esta función
-ingresa el dato en la i-ésima posición de la lista (i=0 es la primera posición).
-
-    Si el arreglo está lleno, primero debe aumentar su capacidad al doble.
-
-    Recuerde mover los elementos siguientes una casilla a la derecha.
-
-    Si i es mayor a size, la función no debe hacer nada.
-
-*/
 void push(ArrayList *l, void *data, int i) {
-  if (l->size == l->capacity)
+  if (l->size == l->capacity) // si el array esta lleno
     doblarcapacity(l);
-  if (i > l->size)
+  if (i > l->size) // si el indice es mayor al tamaño del array
     return;
-  if (i == l->size) {
+  if (i == l->size) { // si el indice es igual al tamaño del array
     append(l, data);
     return;
   }
 
-    // si es que i < l->size
-  void *aux1 = NULL;
+  // si el indice es menor al tamaño del array
+  void *aux1 = NULL;  // variable auxiliar
   int resto = l->size - i ;
-  for(int k = 0 ; k  < resto ; k++){
-    aux1 = l->data[i+k];
-    l->data[i+k] = data;
-    data = aux1;
+  for(int k = 0 ; k  < resto ; k++){ // recorrer el array desde el indice hasta el final
+    aux1 = l->data[i+k]; // guardar el valor del elemento en aux1
+    l->data[i+k] = data; // guardar el valor de data en el elemento
+    data = aux1; // guardar el valor de aux1 en data
   }
 
 }
 
-/*
 
-Implemente la función void* pop(ArrayList * l, int i). Esta función elimina y
-retorna el dato de la posición i de la lista. Valores negativos corresponden a
-los datos obtenidos desde el final al principio de la lista (vea la función
-get).
-
-    Recuerde que al eliminar un dato, debe mover los elementos que se encuentran
-a la derecha, una casilla hacia la izquierda
-
-*/
-void *pop(ArrayList *l, int i) {
-  if (i > l->size)
+void *pop(ArrayList *l, int i) { // eliminar un elemento del array
+  if (i > l->size) // si el indice es mayor al tamaño del array
     return NULL;
 
-  void *valor_eleminado = l->data[i];
+  void *valor_eleminado = l->data[i]; // guardar el valor del elemento a eliminar
 
-  if (i == l->size) {
+  if (i == l->size) { // si el indice es igual al tamaño del array
     l->data[l->size] = NULL;
     l->size--;
     return valor_eleminado;
   }
 
-  l->data[i] = NULL;
-  int resto = l->size - i;
-  for (int j = 0; j < resto; j++) {
-    l->data[i + j] = l->data[i + j + 1];
+  l->data[i] = NULL; // eliminar el elemento
+  int resto = l->size - i; // calcular el resto de elementos
+  for (int j = 0; j < resto; j++) { // recorrer el resto de elementos
+    l->data[i + j] = l->data[i + j + 1]; // mover los elementos
   }
-  l->size--;
+  l->size--; // disminuir el tamaño del array
   return valor_eleminado;
 }
 
-/*Implemente la función void* get(ArrayList * l, int i). Esta función retorna el
- * dato de la posición i de la lista. Si i>=size, entonces retorna NULL. Además,
- * si i<0 los valores son obtenidos desde el final hacia el principio de la
- * lista.
- */
-void *get(ArrayList *l, int i) {
-  if (i < 0)
+
+void *get(ArrayList *l, int i) { // obtener un elemento del array
+  if (i < 0) // si el indice es negativo
     i = l->size + i;
 
-  if (i >= l->size)
+  if (i >= l->size) // si el indice es mayor al tamaño del array
     return NULL;
 
-  return l->data[i];
+  return l->data[i]; // retornar el elemento
 }
 
-int get_size(ArrayList *l) {   
+int get_size(ArrayList *l) {   // obtener el tamaño del array
   return l->size; 
 
 }
-/*
 
-    Implemente la función void clean(ArrayList * list). Esta función inicializa
-   la capacidad y tamaño de la lista a sus valores iniciales y reduce el arreglo
-   data a su tamaño inicial (2).
 
-*/
-void clean(ArrayList *l) {
+void clean(ArrayList *l) { // limpiar el array de data
     // hacer un free de cada elemento del array y luego del array 
     // y por ultimo hacer un malloc de nuevo
   for (int i = 0; i < l->size; i++){
     free(l->data[i]);
   }
     // habia una alternativa que me soluciono el problema inicialmente l->data = NULL;
-  l->data = (void **)malloc(sizeof(void *) * 2);
-  l->capacity = 2;
+  l->data = (void **)malloc(sizeof(void *) * 2); // reservar memoria para el array de punteros
+  l->capacity = 2; 
   l->size = 0;
     
 
